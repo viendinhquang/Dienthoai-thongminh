22bc1655e8c8c7711211597bce556233ad912bce
Quang Vien <vienvip9x@gmail.com>
2017-09-19 18:05:28 +0700

    update times 6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
                  .order('created_at DESC')
   end
   def show
+    # binding.pry
     @post = Post.order("created_at ASC").limit(4)
     @post_show = Post.find(params[:id])
     # @post_comment = PostComment.create(user_id: current_user.id, post_id: params[:post_comment][:post_id], content: params[:content])
